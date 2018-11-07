432a50f35eeaa621ae57acafd5189c34647b4f6e
wfengcd <1251910322@qq.com>
2018-11-07 08:53:31 +0800

    在根url显示首页内容

--- server04.py
+++ server04.py
@@ -1,12 +1,51 @@
 from http.server import BaseHTTPRequestHandler, HTTPServer
 import os, sys
 
+class case_no_files(object):
+	#handler == RequestHandler的self
+	def test(self, handler):
+		return not os.path.exists(handler.full_path)
+		
+	def act(self, handler):
+		raise ServerException("'{0}' not found".format(handler.path))
+
+class case_existing_files(object):
+	def test(self, handler):
+		return os.path.isfile(handler.full_path)
+	def act(self, handler):
+		handler.handler_file(handler.full_path)
+		
+class case_always_fail(object):
+	def test(self, handler):
+		return True
+		
+	def act(self, handler):
+		raise ServerException("Unkown object '{0}'".format(handler.path))
+		
+class case_directory_index_file(object):
+	def index_path(self, handler):
+		return os.path.join(handler.full_path, 'index.html')
+	
+	def test(self, handler):
+		return os.path.isdir(handler.path) and \
+				os.path.isfile(self.index_path(handler))
+	
+	def act(self, handler):
+		handler.handler_file(self.index_path(handler))		
+		
 class ServerException(Exception):
 	'''服务器内部错误'''
 	pass
 
 class RequestHandler(BaseHTTPRequestHandler):
 	
+	Cases = [
+				case_no_files(),
+				case_existing_files(),
+				case_directory_index_file(),
+				case_always_fail(),
+			]
+
 	Error_Page = """\
     <html>
     <body>
@@ -39,28 +78,22 @@ class RequestHandler(BaseHTTPRequestHandler):
 		}
 		page = self.Error_Page.format(**values)
 		self.send_content(page.encode('utf-8'), 404)
-		
+	
 	def do_GET(self):
 		try:
 			#os.getcwd() 是当前的工作目录，self.path 保存了请求的相对路径
 			#不要忘了 RequestHandler 继承自 BaseHTTPRequestHandler
 			#它已经帮我们将请求的相对路径保存在self.path中了。
-			full_path = os.getcwd()+self.path
-			#如果路径不存在...
-			if not os.path.exists(full_path):
-				#抛出异常：文件未找到
-				raise ServerException("'{0}' not found".format(self.path))
-			#是一个路径文件
-			elif os.path.isfile(full_path):
-				#调用handler处理文件
-				self.handler_file(full_path)
-			else:
-				#抛出异常路径为不知名对象
-				raise ServerException("Unkown object '{0}'".format(self.path))
+			self.full_path = os.getcwd()+self.path
+			
+			for case in self.Cases:
+				if case.test(self):
+					case.act(self)
+					break
 				
 		except Exception as msg:
 			self.handler_error(msg)
-		
+			
 if __name__ == '__main__':
 	serverAddress = ('', 8080)
 	server = HTTPServer(serverAddress, RequestHandler)
